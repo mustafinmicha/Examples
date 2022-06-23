670d07b04f2d33985d854bb172d52bc701bef89f
login <mus666@yandex.ru>
2022-06-23 23:55:24 +1000

    Example 006 If-Else

---
+++ Example006_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+      Console.WriteLine("Ура, это же МАША!");
+}
+elce  
+{
+      Console.Write("Привет,");
+      Console.WriteLine(username);
+}     
